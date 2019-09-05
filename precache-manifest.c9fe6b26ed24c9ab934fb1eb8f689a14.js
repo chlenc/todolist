6ef2689ca072c5af2e666c7470157249625e750e
Alexey Nagorny <anagorny@wavesplatform.com>
2019-09-05 14:16:58 +0300

    Updates

---
+++ precache-manifest.c9fe6b26ed24c9ab934fb1eb8f689a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3b03f397d5ef65c419aa7c8abe20dd",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2258c1fbd5c136d9e063",
+    "url": "/todolist/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "bbcc243dee36df042d60",
+    "url": "/todolist/static/css/main.65654a0c.chunk.css"
+  },
+  {
+    "revision": "2258c1fbd5c136d9e063",
+    "url": "/todolist/static/js/2.d0a307e6.chunk.js"
+  },
+  {
+    "revision": "bbcc243dee36df042d60",
+    "url": "/todolist/static/js/main.1010d627.chunk.js"
+  },
+  {
+    "revision": "494ea65bfa3a7b566acf",
+    "url": "/todolist/static/js/runtime~main.b0077857.js"
+  }
+]);
